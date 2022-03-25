1837be8701e75b2a8b8ecb37820de17369e0f9d2
balaji-githubstore <31404824+balaji-githubstore@users.noreply.github.com>
2022-03-25 19:19:59 +0530

    working with frame

---
+++ src/com/nse/seleniumadvance/Demo2FrameUsingNameOrId.java
@@ -0,0 +1,33 @@
+package com.nse.seleniumadvance;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+//using frame webelement 
+public class Demo2FrameUsingNameOrId {
+
+	
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "D:\\Software\\chromedriver.exe");
+
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		
+		driver.get("https://netbanking.hdfcbank.com/netbanking/");
+		
+		driver.switchTo().frame(driver.findElement(By.xpath("//frame[@name='login_page']")));
+		
+		driver.findElement(By.name("fldLoginUserId")).sendKeys("hello");
+		driver.findElement(By.linkText("CONTINUE")).click();
+		
+		driver.switchTo().defaultContent();
+	}
+}
+
+
+
---
+++ src/com/nse/seleniumadvance/Demo3FrameUsingIndex.java
@@ -0,0 +1,35 @@
+package com.nse.seleniumadvance;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+//using frame webelement 
+public class Demo3FrameUsingIndex {
+
+	
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "D:\\Software\\chromedriver.exe");
+
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		
+		driver.get("https://netbanking.hdfcbank.com/netbanking/");
+		
+		driver.switchTo().frame(0);
+		
+		driver.findElement(By.name("fldLoginUserId")).sendKeys("hello");
+		driver.findElement(By.linkText("CONTINUE")).click();
+		
+		driver.switchTo().defaultContent();
+		
+		
+	}
+}
+
+
+
